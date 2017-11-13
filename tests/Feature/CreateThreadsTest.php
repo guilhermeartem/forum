67328b6a8e7147e1d55a268ece3bbd70a37fb83b
Guilherme Artém <guilherme.artem@gmail.com>
2017-11-13 12:38:47 -0200

    Episodio 23/2: A User Can Delete Their Threads

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -61,6 +61,36 @@ class CreateThreadsTest extends TestCase
             ->assertSessionHasErrors('channel_id');
 
     }
+    
+    /** @test */
+    function guests_cannot_delete_threads()
+    {
+        $thread = create('App\Thread');
+
+        $response = $this->delete($thread->path());
+        $response->assertRedirect('/login');
+    }
+    
+    /** @test */
+    function threads_may_onlu_be_deleted_by_those_who_have_permission()
+    {
+        //TODO:
+    }
+    
+    /** @test */
+    function a_thread_can_be_deleted()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+        $reply = create('App\Reply', ['thread_id' => $thread->id]);
+
+        $response = $this->json('DELETE', $thread->path());
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('threads', ['id' => $thread->id]);
+        $this->assertDatabaseMissing('replies', ['id' => $reply->id]);
+    }
 
     public function publishThread($overrides = [])
     {
