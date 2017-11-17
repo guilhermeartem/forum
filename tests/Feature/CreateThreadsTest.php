0fdbc973ade4d81eb843dc301822f8220e06739b
Guilherme Artém <guilherme.artem@gmail.com>
2017-11-17 09:18:15 -0200

    Episodio 24: Authorization With Policies

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -63,26 +63,23 @@ class CreateThreadsTest extends TestCase
     }
     
     /** @test */
-    function guests_cannot_delete_threads()
+    function unauthorized_users_may_not_delete_threads()
     {
         $thread = create('App\Thread');
 
-        $response = $this->delete($thread->path());
-        $response->assertRedirect('/login');
+        $this->delete($thread->path())->assertRedirect('/login');
+
+        $this->signIn();
+        $this->delete($thread->path())->assertStatus(403);
+
     }
     
     /** @test */
-    function threads_may_onlu_be_deleted_by_those_who_have_permission()
-    {
-        //TODO:
-    }
-    
-    /** @test */
-    function a_thread_can_be_deleted()
+    function authorized_users_can_delete_threads()
     {
         $this->signIn();
 
-        $thread = create('App\Thread');
+        $thread = create('App\Thread', ['user_id' => auth()->id()]);
         $reply = create('App\Reply', ['thread_id' => $thread->id]);
 
         $response = $this->json('DELETE', $thread->path());
