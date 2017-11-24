25fb036d45cd055470546666804ebfb846c05aed
Guilherme Artém <guilherme.artem@gmail.com>
2017-11-24 15:20:52 -0200

    Episodio 32: A Vue Reply Component

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::delete('/threads/{channel}/{thread}', 'ThreadsController@destroy');
 
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::delete('/replies/{reply}', 'RepliesController@destroy');
+Route::patch('/replies/{reply}', 'RepliesController@update');
 
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
 
