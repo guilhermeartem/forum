669f86910cda00aca3a4ceadddbaebb8af805576
Guilherme Artém <guilherme.artem@gmail.com>
2017-11-30 09:16:38 -0200

    Episodio 38: A Laravel and Vue Pagination

--- resources/assets/js/components/Replies.vue
+++ resources/assets/js/components/Replies.vue
@@ -1,42 +1,50 @@
 <template>
     <div>
         <div v-for="(reply, index) in items">
-            <reply :data="reply" @deleted="remove(index)"></reply>
+            <reply :data="reply" @deleted="remove(index)" :key="reply.id"></reply>
         </div>
 
-        <new-reply :endpoint="endpoint" @created="add"></new-reply>
+        <paginator :dataSet="dataSet" @changed="fetch"></paginator>
+
+        <new-reply @created="add"></new-reply>
     </div>
 </template>
 
 <script>
     import Reply from './Reply.vue';
     import NewReply from './NewReply.vue';
+    import collection from '../mixins/collection.js'
 
     export default {
         components: { Reply, NewReply },
 
-        props: ['data'],
+        mixins: [collection],
 
         data() {
-            return {
-                items: this.data,
-                endpoint: location.pathname + '/replies'
-            }
+            return { dataSet: false }
+        },
+
+        created() {
+            this.fetch();
         },
 
         methods: {
-            add(reply) {
-                this.items.push(reply);
-
-                this.$emit('added');
+            fetch(page) {
+                axios.get(this.url(page)).then(this.refresh)
             },
 
-            remove(index) {
-                this.items.splice(index, 1);
+            url(page ) {
+                if(! page ) {
+                    let query = location.search.match(/page=(\d+)/);
 
-                this.$emit('removed');
+                    page = query ? query[1] : 1;
+                }
+                return `${location.pathname}/replies?page=${page}`;
+            },
 
-                flash('Reply was deleted!')
+            refresh({data}) {
+                this.dataSet = data;
+                this.items = data.data;
             }
         }
     }
--- resources/assets/js/components/Reply.vue
+++ resources/assets/js/components/Reply.vue
@@ -5,7 +5,7 @@
                 <h5 class="flex">
                     <a :href="'/profiles/'+data.owner.name"
                         v-text="data.owner.name">
-                    </a> said {{ data.created_at }}...
+                    </a> said <span v-text="ago"></span>
                 </h5>
 
                 <div v-if="signedIn">
@@ -35,6 +35,7 @@
 
 <script>
     import Favorite from './Favorite.vue';
+    import moment from 'moment';
 
     export default {
         props: ['data'],
@@ -56,6 +57,10 @@
 
             canUpdate() {
                 return this.authorize(user => this.data.user_id == user.id);
+            },
+
+            ago() {
+                return moment(this.data.created_at).fromNow() + ' ...';
             }
         },
 
