8420cd749eabccce08d3fe6be3c0dbc5fa787c53
Guilherme Artém <guilherme.artem@gmail.com>
2017-12-07 10:34:39 -0200

    Episodio 42: Thread Subscriptions: Part 4

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Notifications\ThreadWasUpdated;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
@@ -48,7 +49,14 @@ class Thread extends Model
 
     public function addReply($reply)
     {
-        return $this->replies()->create($reply);
+        $reply = $this->replies()->create($reply);
+
+        $this->subscriptions
+            ->filter(function ($sub) use ($reply) {
+                return $sub->user->id != $reply->user_id;
+            })->each->notify($reply);
+
+        return $reply;
     }
 
     public function scopeFilter($query, $filters)
@@ -61,6 +69,8 @@ class Thread extends Model
         $this->subscriptions()->create([
             'user_id' => $userId ?: auth()->id()
         ]);
+
+        return $this;
     }
 
     public function unsubscribe($userId = null)
